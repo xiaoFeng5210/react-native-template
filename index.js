6579ee8396d3ff73a289ba790e78a53579da4466
Eadweard <804875728@qq.com>
2024-01-28 20:16:01 +0800

    init

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// 创建文件夹
+import fs from 'node:fs'
+// 创建文件夹 创建入口文件 创建package.json 安装依赖
+
+
+
+
+
+
+fs.mkdirSync('./react-native-cli')
+
+fs.writeFileSync('./react-native-cli/index.js', '内容')
+
+fs.writeFileSync('./react-native-cli/package.json', 'package')
+
